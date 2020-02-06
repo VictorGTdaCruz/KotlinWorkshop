8226df3bd6f7508a86a3c41ac7574f6d4df502ff
Victor Cruz <victorgtdacruz@outlook.com>
2020-02-06 08:51:27 -0300

    Remove first activity code

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -10,14 +10,5 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        compareButton.setOnClickListener {
-            val car1 = Car(firstCarName.text.toString(), firstCarVelocity.text.toString().toInt())
-
-            val car2 = Car(secondCarName.text.toString(), secondCarVelocity.text.toString().toInt())
-
-            result.text = faster(car1, car2).speed.toString()
-        }
     }
-
-    fun faster(first : Car, second: Car): Car = if (first.speed > second.speed) first else second
 }
