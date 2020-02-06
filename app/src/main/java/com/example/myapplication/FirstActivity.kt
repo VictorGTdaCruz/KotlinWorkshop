bd8521d7b89c3cddf55393d6065544629a10ec11
Victor Cruz <victorgtdacruz@outlook.com>
2020-02-06 02:07:56 -0300

    Add second activity

---
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        compareButton.setOnClickListener {
+            val car1 = Car(firstCarName.text.toString(), firstCarVelocity.text.toString().toInt())
+
+            val car2 = Car(secondCarName.text.toString(), secondCarVelocity.text.toString().toInt())
+
+            result.text = faster(car1, car2).speed.toString()
+        }
+    }
+
+    fun faster(first : Car, second: Car): Car = if (first.speed > second.speed) first else second
+}
